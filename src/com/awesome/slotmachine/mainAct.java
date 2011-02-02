6d128b80a5964d2a735a3b57a668fd3159054cd9
Iman <iman@icannhas.com>
2011-02-03 03:53:01 +0800

    changed main activity, xml layout

--- src/com/awesome/slotmachine/mainAct.java
+++ src/com/awesome/slotmachine/mainAct.java
@@ -3,11 +3,15 @@ package com.awesome.slotmachine;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class mainAct extends Activity {
+public class MainAct extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //Tagalog comment:
+        //Yung text na nakikita sa screen ay galing sa res/layout/main.xml 
+        //may feeling ako na hindi siya dynamic.
     }
 }
+
