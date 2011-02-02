184ec493c418fe7fdedcb13618ed0ca5f82a481f
iman encarnacion <iman.encarnacion@gmail.com>
2011-02-03 04:19:57 +0800

    Main activity changed to SlotMain.java

--- src/com/awesome/slotmachine/SlotMain.java
+++ src/com/awesome/slotmachine/SlotMain.java
@@ -3,7 +3,7 @@ package com.awesome.slotmachine;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainAct extends Activity {
+public class SlotMain extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
