71a27fa5d73b983b5bd4fabc92f066ecbb38568b
Joaq <Joaq@localhost>
2011-02-03 19:45:05 +0800

    Extreme commiting part 2.

--- src/com/awesome/slotmachine/SlotMain.java
+++ src/com/awesome/slotmachine/SlotMain.java
@@ -2,17 +2,23 @@ package com.awesome.slotmachine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class SlotMain extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        ImageView joaq = new ImageView(this);
+        //setContentView(R.layout.main);
+        joaq.setImageResource(R.drawable.test);
+        setContentView(joaq);
+        
         //Tagalog comment:
         //Yung text na nakikita sa screen ay galing sa res/layout/main.xml 
         //may feeling ako na hindi siya dynamic.
         //Wak
+        //To Iman: Nakalagay ako ng image? Pero feeling ko di pa siya ganun ka yeah.
     }
 }
 
